e26f30e17ea82371e9ea29a56795ac92f3c08484
Cappy Harwood. <capricesunxd@gmail.com>
2024-12-10 17:54:55 -0700

    no delays

--- FInal_01/src/FInal_01.cpp
+++ FInal_01/src/FInal_01.cpp
@@ -1,19 +1,23 @@
 /* 
- * Project : Final
+ * Project : Strident Straightner 
  * Author: Caprice Harwood
- * Date: December 2024
+ * Date: December of 2024
  */
 
 
 #include "Particle.h"
 #include "Grove-Ultrasonic-Ranger.h"
+#include "IoTTimer.h"
+
 SYSTEM_MODE(SEMI_AUTOMATIC);
 SerialLogHandler logHandler(LOG_LEVEL_INFO);
 Ultrasonic Strident (D3);
 Ultrasonic Ultra (D7);
-// int position,prevpos;
-const int Speaker (D8);
- 
+class IoTTimer SoundTimer;
+float position;
+const int Speaker (A5);
+bool prevP;
+
 
 void setup()
 {
@@ -25,16 +29,33 @@ pinMode(Speaker,OUTPUT);
 void loop()
 {
 	long RangeInInches;
-	long RangeInCentimeters;
+	// long RangeInCentimeters;
 
-	RangeInInches = Strident.MeasureInInches();
-	RangeInCentimeters = Ultra.MeasureInCentimeters();
-	// if(position!=prevpos){
-	Serial.printf("Ultrasonic#1 %i\n",RangeInInches);
-	delay(250);
-	Serial.printf("Ultrasonic#2 %i\n",RangeInCentimeters);
-	// prevpos=position;
+	RangeInInches = Strident.MeasureInInches();//set to inches
+  if(RangeInInches= !position){
+  	Serial.printf("Ultrasonic#:%i\n",RangeInInches);
+    prevP=RangeInInches; //if equal don't print
+	delay(5000);
+   } 
+	RangeInInches = Ultra.MeasureInInches();
+     if(RangeInInches!=prevP){
+  	Serial.printf("Ultrasonic#seccond : %i\n",RangeInInches);
+    prevP=RangeInInches; //if equal don't print
+	delay(5000);
+   } 
+
+	if(Strident!=Ultra){  //if they are not equal sound alarm 
+		SoundTimer.start
+		tone(Speaker,440);
 	}
+	else{
+		if(SoundTimer,isTimerReady());
+		toneOff();
+	}
+}
 	// RangeInCentimeters = ultrasonic.MeasureInCentimeters(); // two measurements should keep an interval
   // Serial.printf("%i \n", RangeInCentimeters);
 	// delay(250);
+
+
+
